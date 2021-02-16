fe30b0dec443ce5d253d3433545f35b03c324992
kiu21 <18024603+kiu21@users.noreply.github.com>
2021-02-16 16:17:42 +0900

    commit

--- App.jsx
+++ App.jsx
@@ -5,10 +5,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import MemoList from './src/components/MemoList';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoListScreen/>
+    <MemoCreateScreen/>
   );
 }
 
